38501d63acfeb806bcdcd18c035f96d18ee28e61
renzo-c <rcnavarrop@gmail.com>
2018-11-05 19:00:27 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import style from "./main.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import postReducer from "./reducers/postReducer";
+
+const store = createStore(postReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
